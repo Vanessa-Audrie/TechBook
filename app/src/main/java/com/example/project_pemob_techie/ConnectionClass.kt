c3da27fd388ffc54c2e99c7e6d72b71062102473
Vanessa-Audrie <vanessaaudrie43@gmail.com>
2024-11-08 19:47:55 +0700

    80% frontend done, sisa tab layout and history

---
+++ app/src/main/java/com/example/project_pemob_techie/ConnectionClass.kt
@@ -0,0 +1,13 @@
+package com.example.project_pemob_techie
+
+class ConnectionClass {
+    public class ConnectionClass {
+
+        protected static String db = "TechBook";
+        protected static String ip = "10.0.2.2";
+        protected static String port = "3306";
+        protected static String username = "root";
+        protected static String password = "123456";
+
+    }
+}
