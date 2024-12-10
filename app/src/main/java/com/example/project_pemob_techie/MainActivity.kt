969163bfcfd41bd5da1d69de7bfd378bf5501beb
Nuradilah22 <128211068+Nuradilah22@users.noreply.github.com>
2024-12-10 20:45:20 +0700

    Pesan commit kamu

--- app/src/main/java/com/example/project_pemob_techie/MainActivity.kt
+++ app/src/main/java/com/example/project_pemob_techie/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_wishlist, R.id.navigation_history, R.id.navigation_account
+                R.id.navigation_home, R.id.navigation_wishlist, R.id.navigation_notifications, R.id.navigation_account
             )
         )
 
