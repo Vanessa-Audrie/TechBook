ee2647cbeafe94766f699ba4d68c1fd1f10de1be
Vanessa-Audrie <vanessaaudrie43@gmail.com>
2024-12-19 20:47:26 +0700

    Version 1.0

--- app/src/main/java/com/example/project_pemob_techie/ui/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/example/project_pemob_techie/ui/splashscreen/SplashScreen.kt
@@ -13,7 +13,7 @@ import com.example.project_pemob_techie.ui.login.LoginSignup
 
 class SplashScreen : AppCompatActivity() {
 
-    private val SPLASH_TIMEOUT: Long = 2000
+    private val SPLASH_TIMEOUT: Long = 3000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
